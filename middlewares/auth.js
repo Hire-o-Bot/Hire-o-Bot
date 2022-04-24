69cda78d8629ab2a7b682c5cba858d023ec3e424
ahmadzaheerdev <ahmadzaheerdev@gmail.com>
2022-04-24 23:13:39 +0500

    Added: Authentication middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const User = require("../database/models/user");
+const jwt = require("jsonwebtoken");
+
+//AUTHENTICATION MIDDLEWARE
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "thisisuserverification");
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!user) {
+      throw new Error("Please Authenticate");
+    }
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(400).send({ err: "Please Authenticate" });
+  }
+};
+
+module.exports = auth;
