087f0194eba0c174266ea304338e83badc0a3ea0
ahmadzaheerdev <ahmadzaheerdev@gmail.com>
2022-04-27 13:35:14 +0500

    Added: New Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import CompanyLogin from "./components/auth/CompanyLogin";
+import CompanyRegister from "./components/auth/CompanyRegister";
+import Navbar from "./components/navigation/Navbar";
 
 function App() {
   return (
     <div>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />}></Route>
+          <Route path='/login/candidate' element={<Login />} />
+          <Route path='/register/candidate' element={<Register />} />
+          <Route path='/login/company' element={<CompanyLogin />} />
+          <Route path='/register/company' element={<CompanyRegister />} />
         </Routes>
       </Router>
     </div>
