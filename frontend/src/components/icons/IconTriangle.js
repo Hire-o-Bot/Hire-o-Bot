04bbaf951aa80843c5bffef69af0c4b9c7291b32
ahmadzaheerdev <ahmadzaheerdev@gmail.com>
2022-04-27 13:27:58 +0500

    Added a new Icon

---
+++ frontend/src/components/icons/IconTriangle.js
@@ -0,0 +1,12 @@
+const IconTriangle = ({ fill }) => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+      <path
+        style={{ fill }}
+        d='M13.48,18.48,22.91,8.25a2,2,0,0,0-1.48-3.37H2.57A2,2,0,0,0,1.09,8.25l9.43,10.23A2,2,0,0,0,13.48,18.48Z'
+      />
+    </svg>
+  );
+};
+
+export default IconTriangle;
--- frontend/src/components/icons/index.js
+++ frontend/src/components/icons/index.js
@@ -2,3 +2,4 @@ export { default as IconEmail } from "./IconEmail";
 export { default as IconLock } from "./IconLock";
 export { default as IconEye } from "./IconEye";
 export { default as IconInvisible } from "./IconInvisible";
+export { default as IconTriangle } from "./IconTriangle";
