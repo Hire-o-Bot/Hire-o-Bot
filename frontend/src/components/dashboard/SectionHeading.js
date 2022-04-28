7fbc9c56d699db3fbfdbf7a4f7bc425e5879e466
ghulamghousdev <itsghulamghous@gmail.com>
2022-04-28 20:13:21 +0500

    Added subheading component

---
+++ frontend/src/components/dashboard/SectionHeading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SectionHeading = (props) => {
+  return (
+    <span className="text-gray-6 text-xs font-semibold">{props.heading}</span>
+  );
+};
+
+export default SectionHeading;
