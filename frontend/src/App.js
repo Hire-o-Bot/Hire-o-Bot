2b12d9294caab1a5fed416853fe1ce4d7c5ae3d2
ghulamghousdev <itsghulamghous@gmail.com>
2022-04-28 21:38:55 +0500

    Resolved conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,18 @@ import Register from "./components/auth/Register";
 import CompanyLogin from "./components/auth/CompanyLogin";
 import CompanyRegister from "./components/auth/CompanyRegister";
 import Navbar from "./components/navigation/Navbar";
+import Dashboard from "./components/dashboard/dashboard";
 
 function App() {
   return (
     <div>
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/login/candidate" element={<Login />} />
           <Route path="/register/candidate" element={<Register />} />
           <Route path="/login/company" element={<CompanyLogin />} />
           <Route path="/register/company" element={<CompanyRegister />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
