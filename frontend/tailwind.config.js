d42bc9028355f98761b1b249440f116519e70a86
ghulamghousdev <itsghulamghous@gmail.com>
2022-04-28 20:10:47 +0500

    Added custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "blue-magneta-dark": "#1A1A3C",
+      white: "#ffffff",
+      "gray-6": "#F2F2F2",
+    },
+  },
+  variants: {
+    display: ["group-hover"],
   },
   plugins: [],
 };
