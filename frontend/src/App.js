c14651662a915d44128eda33d705e8bfe05e5eef
ghulamghousdev <itsghulamghous@gmail.com>
2022-04-28 20:09:05 +0500

    Added route for dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/dashboard";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
         </Routes>
       </Router>
     </div>
