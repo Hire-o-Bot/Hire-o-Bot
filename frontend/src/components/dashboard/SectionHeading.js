a769ac592fc52a72dd01fe215124ef5d6bc52a11
ghulamghousdev <itsghulamghous@gmail.com>
2022-05-01 22:24:06 +0500

    Minor changes

--- frontend/src/components/dashboard/SectionHeading.js
+++ frontend/src/components/dashboard/SectionHeading.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const SectionHeading = (props) => {
   return (
-    <span className="text-gray-6 text-xs font-semibold">{props.heading}</span>
+    <span className="text-[#F2F2F2] text-xs font-semibold">
+      {props.heading}
+    </span>
   );
 };
 
