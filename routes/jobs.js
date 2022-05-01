b37f2944510147cde1a250b8a81cb89c57a81e5c
ghulamghousdev <itsghulamghous@gmail.com>
2022-05-01 21:47:25 +0500

    Added route for add job component

---
+++ routes/jobs.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const Jobs = require("../database/models/newJob");
+const router = new express.Router();
+
+// Create a new job
+router.post("/api/addnewjob", async (req, res) => {
+  const job = new Jobs(req.body);
+  try {
+    await job.save();
+    res.status(201).send(job);
+  } catch (err) {
+    res.status(400).send({ err });
+  }
+});
+
+module.exports = router;
