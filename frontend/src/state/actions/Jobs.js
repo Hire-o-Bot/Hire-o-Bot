afd5ac97f877f799d49d6e50ad839bfeaab5d1aa
ghulamghousdev <itsghulamghous@gmail.com>
2022-05-01 22:28:19 +0500

    Added action for Add New Job Component

---
+++ frontend/src/state/actions/Jobs.js
@@ -0,0 +1,41 @@
+import { JOB_CREATION_FAIL, JOB_CREATION_SUCCESS } from "./actionTypes";
+import axios from "axios";
+
+export const addJob =
+  (
+    jobPosition,
+    minExperience,
+    lowerRange,
+    upperRange,
+    jobDescription,
+    technologies
+  ) =>
+  async (dispatch) => {
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = {
+      jobPosition,
+      minExperience,
+      lowerRange,
+      upperRange,
+      jobDescription,
+      technologies,
+    };
+
+    try {
+      const res = await axios.post("/api/addnewjob", body, config);
+      dispatch({
+        type: JOB_CREATION_SUCCESS,
+        payload: res.data,
+      });
+    } catch (err) {
+      console.log(err);
+      dispatch({
+        type: JOB_CREATION_FAIL,
+      });
+    }
+  };
