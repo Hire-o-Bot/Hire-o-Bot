9269a853890e646d55d1d92c2fbcbd5c9c2a128e
ahmadzaheerdev <ahmadzaheerdev@gmail.com>
2022-05-02 16:24:20 +0500

    Populated user

--- routes/company.js
+++ routes/company.js
@@ -6,15 +6,18 @@ const auth = require("../middlewares/auth");
 //ROUTE TO CREATE A NEW COMPANY PROFILE
 router.post("/api/profile/company", auth, async (req, res) => {
   //Checking if the profile already exists
-  const foundCompany = Company.findOne({ user: req.user._id });
+  const foundCompany = await Company.findOne({ user: req.user._id });
   if (foundCompany) {
     return res.status(400).send({ err: "Profile already exists" });
   }
   const company = new Company({ user: req.user._id, ...req.body });
   try {
     await company.save();
+    await Company.populate(company, "user");
     res.status(201).send(company);
   } catch (err) {
     res.status(400).send(err);
   }
 });
+
+module.exports = router;
