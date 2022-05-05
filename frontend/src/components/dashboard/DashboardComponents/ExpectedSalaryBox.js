d6e326d9b94ed56fce13e4cce813a8a1f0c3b539
ghulamghousdev <itsghulamghous@gmail.com>
2022-05-05 18:50:28 +0500

    Created Expected Salary component

---
+++ frontend/src/components/dashboard/DashboardComponents/ExpectedSalaryBox.js
@@ -0,0 +1,32 @@
+import SalaryBox from "../SalaryInputBox";
+import React from "react";
+
+const ExpectedSalary = ({ onChange, lowerRange, upperRange }) => {
+  return (
+    <div className="pl-8 pt-5 w-full pr-8 flex flex-row justify-between items-center">
+      <h1 className="font-medium text-[#333333] text-base ">
+        Expected Salary Between
+      </h1>
+
+      <SalaryBox
+        onChange={onChange}
+        name="lowerRange"
+        value={lowerRange}
+        placeholder="Lower Range"
+        label="Lower Range"
+      />
+
+      <h1 className="font-medium text-[#333333] text-base ">And</h1>
+
+      <SalaryBox
+        onChange={onChange}
+        name="upperRange"
+        value={upperRange}
+        placeholder="Upper Range"
+        label="Upper Range"
+      />
+    </div>
+  );
+};
+
+export default ExpectedSalary;
