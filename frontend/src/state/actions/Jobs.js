9ed41e67256f54a9e45ec04a73a9856e6ae2919f
ghulamghousdev <itsghulamghous@gmail.com>
2022-05-21 19:07:29 +0500

    Added state management to get all the jobs

--- frontend/src/state/actions/Jobs.js
+++ frontend/src/state/actions/Jobs.js
@@ -1,4 +1,12 @@
-import { JOB_CREATION_FAIL, JOB_CREATION_SUCCESS } from "./actionTypes";
+import {
+  JOB_CREATION_FAIL,
+  JOB_CREATION_SUCCESS,
+  LOAD_JOBS_FAIL,
+  LOAD_JOBS_SUCCESS,
+  DELETE_JOB_SUCCESS,
+  DELETE_JOB_FAIL,
+  DELETE_JOBS_SUCCESS,
+} from "./actionTypes";
 import axios from "axios";
 
 export const addJob =
@@ -39,3 +47,24 @@ export const addJob =
       });
     }
   };
+
+export const getJobs = () => async (dispatch) => {
+  const config = {
+    header: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.get("/api/jobs", config);
+    dispatch({
+      type: LOAD_JOBS_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: LOAD_JOBS_FAIL,
+    });
+  }
+};
