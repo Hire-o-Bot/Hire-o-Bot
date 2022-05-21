648fdbca18ddab6c00f2c9ee9d8a9725697fc810
ghulamghousdev <itsghulamghous@gmail.com>
2022-05-21 20:12:40 +0500

    Added reducer for getjobs

--- frontend/src/state/reducers/index.js
+++ frontend/src/state/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import company from "./companyProfile";
+import jobs from "./jobs";
+
 const rootReducer = combineReducers({
   auth,
   company,
+  jobs,
 });
 
 export default rootReducer;
