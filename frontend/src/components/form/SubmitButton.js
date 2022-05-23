88c2e86d4cde3856f61665cef71116d676d5a468
ahmadzaheerdev <ahmadzaheerdev@gmail.com>
2022-05-24 00:41:30 +0500

    Improved Component

--- frontend/src/components/form/SubmitButton.js
+++ frontend/src/components/form/SubmitButton.js
@@ -1,11 +1,20 @@
-const SubmitButton = ({ submitForm, value }) => {
+const SubmitButton = ({
+  submitForm,
+  value,
+  fullWidth = false,
+  alignRight = false,
+}) => {
   return (
-    <input
-      className='w-full p-2.5 mb-4 bg-[#00ed64] text-white font-semibold tracking-wide cursor-pointer hover:bg-[#00ed64] transition rounded-md overflow-hidden'
-      type='submit'
-      onClick={(e) => submitForm(e)}
-      value={value}
-    />
+    <div className={`${alignRight ? "flex justify-end" : null}`}>
+      <input
+        className={`${
+          fullWidth ? "w-full" : "inline-block"
+        } py-2.5 px-4 mb-4 bg-[#00ed64] text-white font-semibold tracking-wide cursor-pointer hover:bg-[#00ed64] transition rounded-md overflow-hidden`}
+        type='submit'
+        onClick={(e) => submitForm(e)}
+        value={value}
+      />
+    </div>
   );
 };
 export default SubmitButton;
