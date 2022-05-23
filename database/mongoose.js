66df1e488f8ada92b1b5ed16923f5f4a9c174321
ghulamghousdev <itsghulamghous@gmail.com>
2022-05-24 01:00:42 +0500

    DB

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 //CONNECTING TO DATABASE
 mongoose
-  .connect()
+  .connect(
+    "mongodb://hireobot:ahmadzaheer32@fyp-shard-00-00.wz5mc.mongodb.net:27017,fyp-shard-00-01.wz5mc.mongodb.net:27017,fyp-shard-00-02.wz5mc.mongodb.net:27017/fyp?ssl=true&replicaSet=atlas-ghms2l-shard-0&authSource=admin&retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => {
     console.log("Connected to database ");
   })
